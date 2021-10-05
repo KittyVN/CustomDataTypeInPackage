fd004ae67163e73b7f76915d223e800932f2f3bd
KittyVN <daniel.schermann12@gmail.com>
2021-10-05 11:20:36 +0200

    Projektdateien hinzufügen.

---
+++ TestApp/App.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO.Compression;
+
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        public void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            if (e.Args.Length == 1) //make sure an argument is passed
+            //if(true)
+            {
+                FileInfo file = new FileInfo(e.Args[0]);
+                //FileInfo file = new FileInfo(@"C:\Users\dschermann\Desktop\test\test.vp");
+                if (file.Exists) //make sure it's actually a file
+                {
+                    using (ZipArchive archive = ZipFile.Open(file.FullName, ZipArchiveMode.Update))
+                    {
+                        ZipArchiveEntry entry = archive.GetEntry("test.txt");
+                        /*using (StreamWriter writer = new StreamWriter(entry.Open()))
+                        {
+                            writer.BaseStream.Seek(0, SeekOrigin.End);
+                            writer.WriteLine("append line to file");
+                        }*/
+                        using (StreamReader reader = new StreamReader(entry.Open()))
+                        {
+                            window.txtText1.Text = reader.ReadToEnd();
+                        }
+                    }
+
+
+                    using (FileStream zipToOpen = new FileStream(file.FullName, FileMode.Open))
+                    {
+                        
+                        using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+                        {
+                            ZipArchiveEntry readmeEntry = archive.CreateEntry("Readme.txt");
+                            using (StreamWriter writer = new StreamWriter(readmeEntry.Open()))
+                            {
+                                writer.WriteLine("Information about this package.");
+                                writer.WriteLine("========================");
+                            }
+                        }
+                    }
+                         window.txtText.Text = "Opened via a file";
+
+                    }
+            }
+            else
+            {
+                window.txtText.Text = "NOT opened via a file";
+            }
+            window.Show();
+
+        }
+    }
+}
---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO.Compression;
+
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+
+                saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                string path = saveFileDialog1.FileName;
+                if(path!= "")
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                    string fileName = "test.txt";
+                    string pathfile;
+                    pathfile = System.IO.Path.Combine(path, fileName);
+                    Console.WriteLine("Path to my file: {0}\n", pathfile);
+
+                    BinaryWriter bw = new BinaryWriter(File.Create(pathfile));
+                    bw.Write("Created with my APP, otherwise not much to read here");
+                    bw.Dispose();
+
+
+                    ZipFile.CreateFromDirectory(path, path + ".vp");
+                    Directory.Delete(path,true);
+
+                }
+
+            }
+        }
+
+        
+
+    }
+    
+}
